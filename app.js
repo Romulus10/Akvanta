27ef4428f5c12030030a8a2624a886057d0b358d
Sean Batzel <stbatzel@gmail.com>
2016-01-02 20:53:20 -0500

    resolved app.js

--- app.js
+++ app.js
@@ -6,6 +6,3 @@ var str = url.substring(nex+1);
 console.log(str);
 var name = str;
 console.log("Started a new game as ") + name;
-document.write("<h1>Akvanta</h1><p id='map'></p>");
-var player = {
-}
