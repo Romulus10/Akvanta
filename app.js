ab36625a2b45cc86403a9fcd81f651f81afa577a
sbatzel <stbatzel@gmail.com>
2016-01-04 11:06:35 -0500

    Refactored for iPhone 5 support.

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 var player = { name: "", health: 0, energy: 0, strength: 0, defense: 0, intellect: 0, level: 0, experience: 0, x: 0, y: 0 };
+var enemyOne = {health:0, strength:1, defense:0};
+var enemyTwo = {health:0, strength:2, defense:1};
+var enemyThree = {health:0, strength:3, defense:2};
+var enemyFour = {health:0, strength:4, defense:3};
+var enemyFive = {health:0, strength:5, defense:4};
+
 var map = [["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
     ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
     ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
@@ -21,7 +27,7 @@ var underMap = [["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"
     ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
     ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
     ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+    ["0", "0", "0", "0", "0", "!", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
     ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
     ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
     ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
@@ -33,19 +39,19 @@ var underMap = [["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"
 
 function createPlayer(str) {
     console.log("Player initializing.");
-    map[player.x][player.y] = "X";
     player = {
         name: str,
         health: 10,
         energy: 0,
-        strength: 3,
+        strength: 1,
         defense: 0,
-        intellect: 2,
+        intellect: 1,
         level: 1,
         experience: 0,
         x: 1,
         y: 1
     };
+    map[player.x][player.y] = "X";
 }
 
 window.onload = function () {
@@ -80,6 +86,7 @@ function drawMap() {
         mapString = mapString + "&#13;&#10;"
     }
     document.getElementById("map").innerHTML = mapString;
+    writeStats();
 }
 
 function writeStats() {
@@ -88,8 +95,12 @@ function writeStats() {
 }
 
 function updateConsole(text) {
-    document.getElementById("console").value = text;
+    var textarea = document.getElementById("console");
+    var current = textarea.value;
+    current = current + "\n" + text;
+    document.getElementById("console").value = current;
     console.log(text);
+    textarea.scrollTop = 99999;
 }
 
 // Button functions.
@@ -98,7 +109,7 @@ function moveUp() {
     map[player.x][player.y] = "x";
     player.x--;
     console.log("Player is now at " + player.x + "," + player.y);
-    moveCheck();
+    turnCheck();
     drawMap();
 }
 
@@ -106,7 +117,7 @@ function moveDown() {
     map[player.x][player.y] = "x";
     player.x++;
     console.log("Player is now at " + player.x + "," + player.y);
-    moveCheck();
+    turnCheck();
     drawMap();
 }
 
@@ -114,7 +125,7 @@ function moveLeft() {
     map[player.x][player.y] = "x";
     player.y--;
     console.log("Player is now at " + player.x + "," + player.y);
-    moveCheck();
+    turnCheck();
     drawMap();
 }
 
@@ -122,13 +133,13 @@ function moveRight() {
     map[player.x][player.y] = "x";
     player.y++;
     console.log("Player is now at " + player.x + "," + player.y);
-    moveCheck();
+    turnCheck();
     drawMap();
 }
 
 // Logic functions.
 
-function moveCheck() {
+function turnCheck() {
     var x = player.x;
     var y = player.y;
     if (x <= 0) {
@@ -147,9 +158,7 @@ function moveCheck() {
         console.log("On the edge. Don't move any further over.");
         updateConsole("There is a wall here. ");
     }
-    else{
-        updateConsole("");
-    }
+    else{}
     map[x][y] = "X"
     try {
         if (map[x - 1][y - 1] != "x") {
@@ -173,11 +182,70 @@ function moveCheck() {
         if (map[x + 1][y] != "x") {
             map[x + 1][y] = underMap[x + 1][y];
         }
-        if (map[x + 1][y + 1] != "x") {
+        if (map[x + 1][y + 1] == "x") {
             map[x + 1][y + 1] = underMap[x + 1][y + 1];
+        }
+        if (map[x - 1][y - 1] == "!") {
+            updateConsole("A trap!");
+            player.health--;
+            map[x - 1][y - 1] = "x";
+        }
+        if (map[x - 1][y] == "!") {
+            updateConsole("A trap!");
+            player.health--;
+            map[x - 1][y] = "x";
+        }
+        if (map[x - 1][y + 1] == "!") {
+            updateConsole("A trap!");
+            player.health--;
+            map[x - 1][y + 1] = "x";
+        }
+        if (map[x][y - 1] == "!") {
+            updateConsole("A trap!");
+            player.health--;
+            map[x][y - 1] = "x";
+        }
+        if (map[x][y + 1] == "!") {
+            updateConsole("A trap!");
+            player.health--;
+            map[x][y + 1] = "x";            
+        }
+        if (map[x + 1][y - 1] == "!") {
+            updateConsole("A trap!");
+            player.health--;
+            map[x + 1][y - 1] = "x";
+        }
+        if (map[x + 1][y] == "!") {
+            updateConsole("A trap!");
+            player.health--;
+            map[x + 1][y] = "x";
+        }
+        if (map[x + 1][y + 1] == "!") {
+            updateConsole("A trap!");
+            player.health--;
+            map[x + 1][y + 1] = "x";
+        }
+        if (player.health < 3){
+            updateConsole("Your health is low!");
+        }
+        if (player.health == 0){
+            alert("You are dead!");
+            window.location.assign("index.html");
         }
     }
     catch (TypeError) {
         console.log("On an edge, should still reveal spots on the board.");
+    }
+}
+
+function heal(){
+    if (player.energy > 0){
+        player.energy--;
+        player.health++;
+        updateConsole("Used a healing spell.");
+        writeStats();
+    }
+    else{
+        updateConsole("Not enough energy!");
     }
 }
