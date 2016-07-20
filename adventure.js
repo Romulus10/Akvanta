9f3411cabde059434fedd2640fe4c9740c30c967
Sean Batzel <stbatzel@gmail.com>
2016-07-19 22:57:24 -0400

    Started trying to get the UI working.

--- adventure.js
+++ adventure.js
@@ -1,3 +1,9 @@
 window.onload = function () {
     console.log("Game starting.")
 };
+
+send_command = function(){
+var command = document.getElementByID("comm").value;
+document.getElementByID("game").value = command;
+console.log(command);
+}
