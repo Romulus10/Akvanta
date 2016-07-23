5e76b207c8db1e77ab96f101b1ffb7a02576ef94
sbatzel <stbatzel@gmail.com>
2016-07-22 21:00:00 -0400

    Did some more stuff.

--- adventure.js
+++ adventure.js
@@ -1,10 +1,41 @@
+var con = "";
+var day;
+var player = {
+    "name": "",
+    "health": 10,
+    "max_health": 10,
+    "magic": 0,
+    "max_magic": 0,
+    "stamina": 5,
+    "max_stamina": 5,
+    "strength": 1,
+    "intellect": 1,
+    "dexterity": 1
+};
+
 window.onload = function () {
-    console.log("Game starting.")
+    console.log("Game starting.");
+    day = Math.floor((Math.random() * 5000) + 1);
+    console.log("Day number set as " + day);
+    push_console_update("Prison.\n");
+    push_console_update("Day " + day + ".\n");
+    push_console_update("A crime you did not commit.\n");
 };
 
 send_command = function(){
     var comm = document.getElementById("comm");
     var game = document.getElementById("game");
     console.log(comm.value);
-    game.value = comm.value;
+    con = con + comm.value;
+    game.value = con;
+};
+
+push_console_update = function (message) {
+    con = con + message;
+    console.log(message);
+    pull_console_update();
+};
+
+pull_console_update = function () {
+    document.getElementById("game").value = con;
 };
